74fa9c398c3d95e73366df85ba5354884865bb68
JaceRogers12 <jacerogers12@gmail.com>
2024-05-10 15:10:48 -0600

    made database, first migration, and seed

--- data/migrations/01-make_cars_table.js
+++ data/migrations/01-make_cars_table.js
@@ -1,7 +1,19 @@
-exports.up = function (knex) {
+
+
+exports.up = async function (knex) {
   // DO YOUR MAGIC
+  await knex.schema.createTable("cars", table => {
+    table.increments("carsId");
+    table.string("vin", 12).notNullable().unique();
+    table.string("make", 20).notNullable()
+    table.string("model", 20).notNullable()
+    table.float("mileage").notNullable()
+    table.string("title", 100)
+    table.string("transmission", 100)
+  })
 };
 
-exports.down = function (knex) {
+exports.down = async function (knex) {
   // DO YOUR MAGIC
+  await knex.schema.dropTableIfExists("cars");
 };
--- data/seeds/01-cars.js
+++ data/seeds/01-cars.js
@@ -1,1 +1,9 @@
 // STRETCH
+exports.seed = async function(knex) {
+    await knex("cars").truncate();
+    await knex("cars").insert([
+        {vin: 2730, make: "Toyota", model: "RAV4", mileage: 1, title: "Something", transmission: "SomethingElse"},
+        {vin: 2983, make: "Takenoko", model: "Panda", mileage: 10, title: "Bamboo", transmission: "Lotus"},
+        {vin: 2329, make: "Growlithe", model: "Jerome", mileage: 100, title: "The Loyal Pokemon", transmission: "Fire"},
+    ])
+}
